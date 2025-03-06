f2d29e73f88c61e16f6dd1711d65606d6b6897ae
orlando kuan <orlandokuanb@gmail.com>
2025-03-05 20:38:43 -0500

    FIX CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     async headers() {
         return [
             {
-                // This applies to all routes
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        // Allow embedding in iframes
-                        value: 'SAMEORIGIN',
-                    },
-                    {
-                        // Required for cross-domain iframe communication
-                        key: 'Content-Security-Policy',
-                        value: "frame-ancestors 'self' *"
-                    }
-                ],
-            },
-            {
-                // Special case for the embed route to allow embedding anywhere
                 source: '/embed',
                 headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "frame-ancestors *",
+                    },
                     {
                         key: 'X-Frame-Options',
                         value: 'ALLOWALL',
                     },
+                ],
+            },
+            {
+                source: '/embed.js',
+                headers: [
                     {
-                        key: 'Content-Security-Policy',
-                        value: "frame-ancestors *"
-                    }
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
                 ],
             },
         ];
